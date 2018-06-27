9750a3ce0ccf4e3abf657cb8bea7d1a7d31633b9
kabita bhandari <kavvy2015@gmail.com>
2018-06-26 20:21:59 -0500

    implementing slug to display user readeable address navigation

--- Gemfile
+++ Gemfile
@@ -1,6 +1,15 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'friendly_id'
+
+
+
+
+
+
+
+
 ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -32,6 +41,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' #mapping rhs with lhs
+
+
+  #get 'pages/about'
+  get 'about-me', to: 'pages#about' #can put any name in lhs about, about-me etc
+  #get 'pages/contact'
+  get 'contact', to: 'pages#contact'
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home' #in localhost home page is shown
+
 end
