ba7d0433dd20827a98282b570eb26db926dccaa8
kabita bhandari <kavvy2015@gmail.com>
2018-07-04 13:08:40 -0500

    added name in sign up form and made it available in strong parameters in controller i.e. application controller for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  #configure_permitted_parameters :: just a name I gave
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    # this syntax is from rails 5. older one wont work
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #sign_up is name used for registration new
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) # account_update is the name that devise uses for edit registration
+  end
 end
