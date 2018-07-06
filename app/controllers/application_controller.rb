e96788db01b0c8cdad095affefbad7b51dbdaed8
kabita bhandari <kavvy2015@gmail.com>
2018-07-06 16:18:58 -0500

    added session tracker eg says thanks from visiting me from fb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 include DeviseWhitelistStrongparams
+
+before_action :my_set_source #just name mysetsource
+
+def my_set_source
+  session[:mero_session] = params[:query] if params[:query] # if says if q is there as parameter do this.
+#query just name it can be  like [:q], [:pot]etc
+end
+
+
 end
