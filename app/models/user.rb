92fae2e8a47f6035c96f4cf71a2eb754624508b9
kabita bhandari <kavvy2015@gmail.com>
2018-07-05 19:42:24 -0500

    added hello message while login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name, :last_name
+
+  def fname
+    self.first_name.split.first # eg User.last=> John Smith then self refers to John Smith. self if not write also ok.
+    #in this case I have taken whole first name so can write up to self.first_name  #self.first_name.split[0]
+  end
+
+  def lname
+    self.last_name.split.first
+    #self.last_name.split[0]
+  end
+
 end
