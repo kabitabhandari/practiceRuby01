caa667e7254570b90591c7cf3479c24d8091ca5c
kabita bhandari <kavvy2015@gmail.com>
2018-07-09 17:09:41 -0500

    implemented helperfor logins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def login_helper
+    if current_user
+     link_to "logout", destroy_user_session_path, method: :delete
+    else   # if no user
+      (link_to "register", new_user_registration_path) +
+      "<br>".html_safe +
+     (link_to "log-in", new_user_session_path)
+    end
+  end
+
 end
